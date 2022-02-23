7f792fac9fec09712587de1acbed469118018d08
Mike <47228528+manormand@users.noreply.github.com>
2022-02-23 13:40:22 -0600

    Introduced RPY and ZYZ translation
    Needs work tho

--- hw1.m
+++ hw1.m
@@ -6,3 +6,15 @@
 clear; clc
 
 %% Problem 1
+% define givens
+theta = deg2rad(30);
+w = [0; 0.866; 0.5];
+
+% Get Euler Angles
+rotm = Rotation.axangle2rotm(w,theta);
+Phi = Rotation.rotm2euler(rotm, 'RPY');
+
+% check validity
+Phi_act = [0.0644047, 0.4478289, 0.2810408]';
+tolerance = 0.001;
+rotm_check = abs(Phi-Phi_act) <= tolerance
