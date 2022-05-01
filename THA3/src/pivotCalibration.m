aebdc683e86e68c79596ed7ff8b369b72a35943d
Mike <47228528+manormand@users.noreply.github.com>
2022-05-01 15:40:28 -0500

    misc

--- THA3/src/pivotCalibration.m
+++ THA3/src/pivotCalibration.m
@@ -1,13 +1,3 @@
-function pivotCalibration(Fk)
-n = 1
-A = [];
-b = [];
-for k = 1:n
-    R = Fk(1:3,1:3,k);
-    p = Fk(1:3,4,k);
-    
-    A = [A; R -eye(3)];
-    b = [b; -p];
-end
+function pivotCalibration(df)
 
-A\b
+b = df.cal
