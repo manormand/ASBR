f84ea4c0cc511634297e1b4075b8e87e837c9ee7
pranavkal <99471159+pranavkal@users.noreply.github.com>
2022-05-11 23:35:17 -0500

    PA4

---
+++ THA4/PA4.m
@@ -0,0 +1,11 @@
+%% ASBR - PA2.m
+% Programming Assignment 2 - Electric Boogaloo
+%
+% Authors: Pranav Kalyani, pk7683
+%          Michael Normand, man2837 
+% Date: May 12 2022
+%%
+clear; clc; close all;
+addpath("src\") % adds src directory containing functions
+% urdf_file = 'urdf/kuka_iiwa7_URDF.xacro';
+% robot = getRobotFromURDF(urdf_file);
